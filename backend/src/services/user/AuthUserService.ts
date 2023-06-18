f775ac7cd237fc3cb07079c0f1b578de115b132e
Pablo Sousa <pablo.ufopa@gmail.com>
2023-06-18 19:15:29 -0300

    Login e geração de token JWT

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,6 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
-
+import {sign} from 'jsonwebtoken'
 
 interface AuthRequest{
     email: string;
@@ -28,9 +28,27 @@ class AuthUserService{
             throw new Error("Usuário ou senha incorreta")
         }
 
-        
 
-        return {ok: true}
+        //gerar token JWT e devolver os dados do usuário
+        const token = sign(
+            {
+               name: user.name,
+               email: user.email 
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
+
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
     }
 }
 
