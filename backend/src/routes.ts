64add74b374e440d523fec95970051071453818e
Pablo Sousa <pablo.ufopa@gmail.com>
2023-06-18 15:41:34 -0300

    Criando Migration e Models com Prisma

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,7 @@ import {Router, Request, Response} from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-    return res.json({ nome: 'MyFood' })
+    throw new Error('Erro ao fazer essa requisição.')
 })
 
 export { router };
