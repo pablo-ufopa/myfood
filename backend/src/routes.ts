65e7e023d791625ee6fb808c9cb15b0cc5e8e9b0
Pablo Sousa <pablo.ufopa@gmail.com>
2023-06-22 17:57:39 -0300

    Cadastro de produtos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import multer from 'multer';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
@@ -7,10 +8,15 @@ import { DetailUserController } from './controllers/user/DetailUserController';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 
+import { CreateProductController } from './controllers/product/CreateProductController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated';
+
+import uploadConfig from './config/multer'
 
 const router = Router();
 
+const upload = multer(uploadConfig.upload("./tmp"));
 
 //-- rotas user --
 router.post('/users', new CreateUserController().handle)
@@ -21,5 +27,8 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 //-- rotas category --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/show-categories', isAuthenticated, new ListCategoryController().handle)
+
+//-- rotas product --
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 export { router };
