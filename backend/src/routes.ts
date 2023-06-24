b2b72574052bce06e44ff71e087e0c73b02fa998
Pablo Sousa <pablo.ufopa@gmail.com>
2023-06-24 17:20:33 -0300

    Enviar pedido e listar pedidos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -16,6 +16,8 @@ import { RemoveOrderController } from './controllers/order/RemoveOrderController
 
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController'
+import { ListOrdersController } from './controllers/order/ListOrdersController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -45,6 +47,8 @@ router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/remove-order', isAuthenticated, new RemoveOrderController().handle)
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle )
+router.get('/orders', isAuthenticated, new ListOrdersController().handle )
 
 
 export { router };
