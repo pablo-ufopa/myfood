7ccf1d48819866b1ae1a4e40990265a88768247b
Pablo Sousa <pablo.ufopa@gmail.com>
2023-06-26 18:56:29 -0300

    Criando tela de login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,45 @@
+import Head from "next/head"
+import Image from "next/image";
+import styles from '../../styles/home.module.scss';
 
+import logoImg from '../../public/LOGO.png'
+
+import { Input } from "../components/ui/Input";
+import { Button } from "../components/ui/Button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>MyFood</h1>
+    <>
+    <Head>
+      <title>MyFood - Faça o login</title>
+    </Head>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo MyFood"/>
+
+      <div className={styles.login}>
+        <form>
+
+          <Input
+          placeholder="Digite seu email"
+          type="text"
+          />
+          <Input
+          placeholder="Senha"
+          type="password"
+          />
+
+          <Button
+            type = "submit"
+            loading={false}
+          >
+            Acessar
+          </Button>
+          
+        </form>
+
+        <a className={styles.text}>Ainda não possui uma conta? Cadastre-se aqui! </a>
+      </div>
     </div>
+    </>
   )
 }
